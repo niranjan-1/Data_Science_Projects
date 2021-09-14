a3615b3a994b2d65e054a25c09ef7cb23b24fd1b
Niranjan Kumar <87664518+niranjan-1@users.noreply.github.com>
2021-09-14 15:51:21 +0530

    Add files via upload
    This is and Machine Learning App deployed through "Streamlit" and predicts the salary if one can provide the education level and years of experience in coding and the country.
    DataSet Link:
    https://drive.google.com/open?id=1dfGerWeWkcyQ9GX9x20rdSGj7WtEpzBB

---
+++ SalaryPrediction/predict_page.py
@@ -0,0 +1,53 @@
+#Import
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open(r'C:\Users\sri\Desktop\MLapp\saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("Software Developer Salary Prediction")
+    st.write("""### We Need Some Information to prediict salary""")
+    countries = ('United States',
+                 'India',
+                 'United Kingdom',
+                 'Germany',
+                 'Canada',
+                 'Brazil',
+                 'France',
+                 'Spain',
+                 'Australia',
+                 'Netherlands',
+                 'Poland',
+                 'Italy',
+                 'Russian Federation',
+                 'Sweden'
+                )
+    education = ('Bachelor’s degree', 
+                 'Master’s degree', 
+                 'Less than a Bachelor',
+                 'Post grad'
+                )
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+    experience = st.slider('Years of Experience',0,50,3)
+    ok = st.button("Calculate Salary")
+    
+    if ok:
+        X = np.array([[country, education, experience ]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+        
+        salary = regressor.predict(x)
+        st.subheader(f'The estimated salary is ${0}'.format(salary))
+        
